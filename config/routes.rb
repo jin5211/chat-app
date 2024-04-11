452941b3dab8e30bd1cad6311926b2e5a91d9940
Jin <zinkoko5211@gmail.com>
2024-04-12 07:37:46 +0900

    メッセージ送信機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :rooms, only: [:new, :create]
+  resources :rooms, only: [:new, :create] do
+    resources :messages, only: [:index, :create]
+  end
   root to: "rooms#index"
 end
